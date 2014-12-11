0ce624555fcc64f7a0e39a50fd216cf6a3a26e71
walkboy <walkboy0602@gmail.com>
2014-12-11 17:29:42 +0800

    Add facebook share button in Detail Page

--- App.Ads/App.Ads/Controllers/BaseController.cs
+++ App.Ads/App.Ads/Controllers/BaseController.cs
@@ -26,6 +26,7 @@ namespace App.Ads.Controllers
             ViewBag.SiteName = "9street.my";
             ViewBag.SiteTitle = "Malaysia Free Classified Ads";
 
+            ViewBag.OG_URL = Request.Url.AbsoluteUri;
             ViewBag.OG_Title = "Malaysia Free Classified Ads";
             ViewBag.OG_Desc = "Search thousands of stores, brands, products and user classifieds all in one place!";
             ViewBag.OG_Image = "https://bazaarstorage.blob.core.windows.net/image/common/fblogo3.jpg";
