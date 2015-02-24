5f781047677eacceecf52713a7609a855d9e454a
walkboy-laptop <walkboy0602@gmail.com>
2015-02-24 13:21:20 +0800

    Increase Max Length for Description to 15000 characters

--- App.Ads/App.Core/ViewModel/EditListingViewModel.cs
+++ App.Ads/App.Core/ViewModel/EditListingViewModel.cs
@@ -22,11 +22,11 @@ namespace App.Core.ViewModel
         public int CategoryId { get; set; }
 
         [Required(ErrorMessage = "Please enter Title.")]
-        [StringLength(50, MinimumLength = 10, ErrorMessage = "Minimun {2} charaters, Max {1} characters.")]
+        [StringLength(50, ErrorMessage = "Max {1} characters.")]
         public string Title { get; set; }
 
         [Required]
-        [StringLength(2000, MinimumLength=10, ErrorMessage="Minimun {2} charaters, Max {1} characters.")]
+        [StringLength(15000, MinimumLength=10, ErrorMessage="Minimun {2} charaters, Max {1} characters.")]
         [AllowHtml]
         public string Description { get; set; }
 
